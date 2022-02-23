835d5da8f9bbc2151877a385add58b4ef1ee68ca
gigglop <zaytsevantosio@gmail.com>
2022-02-24 02:02:55 +0300

    docker create database script updated

--- lesson5/docker_create_databases.sh
+++ lesson5/docker_create_databases.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker exec -it my_postgres_source psql -U root -c "create database my_source_database"
 docker exec -it my_postgres_target psql -U root -c "create database my_target_database"
+
 docker cp tcph/dss.ddl my_postgres_source:/
+docker cp tcph/dss.ddl my_postgres_target:/
 docker cp tcph/customer.tbl my_postgres_source:/
 docker cp tcph/lineitem.tbl my_postgres_source:/
 docker cp tcph/nation.tbl my_postgres_source:/
@@ -10,7 +12,10 @@ docker cp tcph/part.tbl my_postgres_source:/
 docker cp tcph/partsupp.tbl my_postgres_source:/
 docker cp tcph/region.tbl my_postgres_source:/
 docker cp tcph/supplier.tbl my_postgres_source:/
+
 docker exec -it my_postgres_source psql my_source_database -f dss.ddl
+docker exec -it my_postgres_target psql my_target_database -f dss.ddl
+
 docker exec -it my_postgres_source psql my_source_database -c "\copy customer FROM '/customer.tbl' CSV DELIMITER '|'"
 docker exec -it my_postgres_source psql my_source_database -c "\copy lineitem FROM '/lineitem.tbl' CSV DELIMITER '|'"
 docker exec -it my_postgres_source psql my_source_database -c "\copy nation FROM '/nation.tbl' CSV DELIMITER '|'"
