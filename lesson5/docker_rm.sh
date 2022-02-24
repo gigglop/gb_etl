34666cf65ae31e3be2f2e4e41a3c7a33644e4d65
gigglop <zaytsevantosio@gmail.com>
2022-02-25 00:34:05 +0300

    docker rm bash script created, readme added

---
+++ lesson5/docker_rm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source .env
+docker-compose down
+# shellcheck disable=SC2046
+docker rm -f $(docker ps -q -f name=$SOURCE_DB_DOCKER_CONTAINER_NAME && docker ps -q -f name=$TARGET_DB_DOCKER_CONTAINER_NAME)
+# shellcheck disable=SC2046
+docker volume rm $(docker volume ls -q -f name=$SOURCE_DB_NAME && docker volume ls -q -f name=$TARGET_DB_NAME)
