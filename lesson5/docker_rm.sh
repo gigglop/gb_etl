8509f6bc1bf1beb90ddc4d2131e17d97144f0c3e
gigglop <zaytsevantosio@gmail.com>
2022-02-25 01:47:51 +0300

    docker_creation updated, readme updated

--- lesson5/docker_rm.sh
+++ lesson5/docker_rm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source .env
 docker-compose down
 # shellcheck disable=SC2046
 docker rm -f $(docker ps -q -f name="$SOURCE_DB_DOCKER_CONTAINER_NAME" && docker ps -q -f name="$TARGET_DB_DOCKER_CONTAINER_NAME")
