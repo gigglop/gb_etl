3f5bd669f079e401457d2829ba2d8553ebb11956
gigglop <zaytsevantosio@gmail.com>
2022-02-25 00:55:26 +0300

    bash scripts updated

--- lesson5/docker_rm.sh
+++ lesson5/docker_rm.sh
@@ -4,4 +4,4 @@ docker-compose down
 # shellcheck disable=SC2046
 docker rm -f $(docker ps -q -f name="$SOURCE_DB_DOCKER_CONTAINER_NAME" && docker ps -q -f name="$TARGET_DB_DOCKER_CONTAINER_NAME")
 # shellcheck disable=SC2046
-docker volume rm $(docker volume ls -q -f name="$SOURCE_DB_NAME" && docker volume ls -q -f name="$TARGET_DB_NAME")
+docker volume rm -f $(docker volume ls -q -f name="$SOURCE_DB_NAME" && docker volume ls -q -f name="$TARGET_DB_NAME")
