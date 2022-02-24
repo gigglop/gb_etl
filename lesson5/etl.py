299aa8565fecaf2d5e0f91edb9f24fb9f205cc51
gigglop <zaytsevantosio@gmail.com>
2022-02-25 02:24:18 +0300

    etl.py finished

--- lesson5/etl.py
+++ lesson5/etl.py
@@ -14,9 +14,29 @@ target_conn_string = f"host='localhost' " \
                      f"dbname='{config.get('TARGET_DB_NAME')}' " \
                      f"user='{config.get('DB_USER')}' " \
                      f"password='{config.get('DB_PASSWORD')}'"
+dirname = 'temp_etl_data/'
+try:
+    with psycopg2.connect(source_conn_string) as conn, conn.cursor() as cursor:
+        query = """
+        SELECT table_name 
+        FROM information_schema.tables
+        WHERE table_schema='public' 
+        """
+        cursor.execute(query)
+        source_tables = cursor.fetchall()
+        table_file_map = {}
+        for table in source_tables:
+            table_file_map[table[0]] = f"{table[0]}.csv"
+            q = f"COPY {table[0]} TO STDOUT WITH DELIMITER ',' CSV HEADER;"
+            with open(f'{dirname}{table[0]}.csv', 'w') as f:
+                cursor.copy_expert(q, f)
 
-with psycopg2.connect(source_conn_string) as conn, conn.cursor() as cursor:
-    query = 'select * from customer limit 1' # запрос к БД
-    cursor.execute(query) # выполнение запроса
-    result = cursor.fetchall() # получение результата
-    print(result)
+    with psycopg2.connect(target_conn_string) as conn, conn.cursor() as cursor:
+        for table in table_file_map.keys():
+            q = f"COPY {table} from STDIN WITH DELIMITER ',' CSV HEADER;"
+            print(q)
+            with open(f'{dirname}{table_file_map[table]}', 'r') as f:
+                cursor.copy_expert(q, f)
+    print('ETL finished.')
+except Exception as e:
+    print(f'Stopped. Reason: {e}')
