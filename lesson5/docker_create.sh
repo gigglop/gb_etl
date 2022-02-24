044da067c8876f2ef2e9116c43f772ff275d96f0
gigglop <zaytsevantosio@gmail.com>
2022-02-25 00:59:46 +0300

    union two in one bash script

--- lesson5/docker_create.sh
+++ lesson5/docker_create.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 source .env
 
+docker volume create --name="$SOURCE_DB_NAME"
+docker volume create --name="$TARGET_DB_NAME"
+docker-compose up -d
+docker ps
+
 docker exec -it "$SOURCE_DB_DOCKER_CONTAINER_NAME" psql -U "$DB_USER" -c "create database $SOURCE_DB_NAME"
 docker exec -it "$TARGET_DB_DOCKER_CONTAINER_NAME" psql -U "$DB_USER" -c "create database $TARGET_DB_NAME"
 
--- lesson5/docker_create_containers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source .env
-
-docker volume create --name="$SOURCE_DB_NAME"
-docker volume create --name="$TARGET_DB_NAME"
-docker-compose up -d
