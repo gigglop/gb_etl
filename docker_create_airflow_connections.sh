4324ca753633b38662c3876fbf77eeefcd0bdca4
gigglop <zaytsevantosio@gmail.com>
2022-10-08 03:04:23 +0300

    airflow connections creation updated

--- docker_create_airflow_connections.sh
+++ docker_create_airflow_connections.sh
@@ -2,20 +2,22 @@
 source .env
 
 docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
-airflow connections add '$SOURCE_DB_NAME' \
-    --conn-type 'Postgres' \
-    --conn-login '$DB_USER' \
-    --conn-password '$DB_PASSWORD' \
-    --conn-host 'db' \
-    --conn-port '5432' \
-    --conn-schema '$SOURCE_DB_NAME'
+airflow connections -a \
+    --conn_id '$SOURCE_DB_NAME' \
+    --conn_type 'Postgres' \
+    --conn_login '$DB_USER' \
+    --conn_password '$DB_PASSWOaRD' \
+    --conn_host 'db' \
+    --conn_port '5432' \
+    --conn_schema '$SOURCE_DB_NAME'
 "
 docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
-airflow connections add '$TARGET_DB_NAME' \
-    --conn-type 'Postgres' \
-    --conn-login '$DB_USER' \
-    --conn-password '$DB_PASSWORD' \
-    --conn-host 'db2' \
-    --conn-port '5432' \
-    --conn-schema '$TARGET_DB_NAME'
+airflow connections -a \
+    --conn_id '$TARGET_DB_NAME' \
+    --conn_type 'Postgres' \
+    --conn_login '$DB_USER' \
+    --conn_password '$DB_PASSWORD' \
+    --conn_host 'db2' \
+    --conn_port '5432' \
+    --conn_schema '$TARGET_DB_NAME'
 "
