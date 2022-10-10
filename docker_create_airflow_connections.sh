d56e9a93b1c8c6d1851b87d1c3a9e27d59ed07e1
gigglop <zaytsevantosio@gmail.com>
2022-10-11 00:30:04 +0300

    airflow connections fixed: conn_type changed to postgres from Postgres

--- docker_create_airflow_connections.sh
+++ docker_create_airflow_connections.sh
@@ -4,7 +4,7 @@ source .env
 docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
 airflow connections -a \
     --conn_id '$AIRFLOW_SOURCE_CONNECTION_NAME' \
-    --conn_type 'Postgres' \
+    --conn_type 'postgres' \
     --conn_login '$DB_USER' \
     --conn_password '$DB_PASSWORD' \
     --conn_host 'db' \
@@ -14,7 +14,7 @@ airflow connections -a \
 docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
 airflow connections -a \
     --conn_id '$AIRFLOW_TARGET_CONNECTION_NAME' \
-    --conn_type 'Postgres' \
+    --conn_type 'postgres' \
     --conn_login '$DB_USER' \
     --conn_password '$DB_PASSWORD' \
     --conn_host 'db2' \
