87566319d4b05b0e6067c35f0e5b2eda9be7493e
gigglop <zaytsevantosio@gmail.com>
2022-10-11 23:33:09 +0300

    updated airflow connections adding

--- docker_create_airflow_connections.sh
+++ docker_create_airflow_connections.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 source .env
 
-docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
-airflow connections -a \
-    --conn_id '$AIRFLOW_SOURCE_CONNECTION_NAME' \
-    --conn_type 'postgres' \
-    --conn_login '$DB_USER' \
-    --conn_password '$DB_PASSWORD' \
-    --conn_host 'db' \
-    --conn_port '5432' \
-    --conn_schema '$SOURCE_DB_NAME'
-"
+docker exec -it $AIRFLOW_DOCKER_CONTAINER_NAME sh -c "export AIRFLOW_CONN_$AIRFLOW_SOURCE_CONNECTION_NAME=\
+  'postgresql://$DB_USER:$DB_PASSWORD@db:5432/$SOURCE_DB_NAME'"
+
 docker exec -it "$AIRFLOW_DOCKER_CONTAINER_NAME" sh -c "
 airflow connections -a \
     --conn_id '$AIRFLOW_TARGET_CONNECTION_NAME' \
