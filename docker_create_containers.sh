aa929b2df01d9a1e81574d4f052a77ee7cd8c398
gigglop <zaytsevantosio@gmail.com>
2022-10-12 22:43:30 +0300

    fernet error InvalidToken fixed

--- docker_create_containers.sh
+++ docker_create_containers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 source .env
+# shellcheck disable=SC2155
+export FERNET_KEY=$(python3 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
+
 docker volume create --name=$SOURCE_DB_NAME
 docker volume create --name=$TARGET_DB_NAME
 docker compose up -d
+
+unset FERNET_KEY
